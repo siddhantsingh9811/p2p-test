440518b738674f607ba797460da67f4869b3d123
Captain Cuckoo <ssiddhant9811@gmail.com>
2022-06-30 13:46:20 +0530

    this is a fucking dumpster fire

---
+++ main.js
@@ -0,0 +1,53 @@
+let peer = new Peer();
+let myId = "loading";
+var conn = null;
+
+peer.on('open',id=>{
+  console.log(id);
+  let yourid = document.getElementById("userid");
+  yourid.innerHTML = `<span>${id}</span>`;
+  myId = id;
+});
+
+let connectButton = document.getElementById("connect")
+connectButton.addEventListener('click', connect);
+
+function connect(){
+  let peerId = document.getElementById("peerid").value;
+  // console.log(text);
+  conn = peer.connect(peerId);
+  conn.on('open', function(){
+    showMessage("CONNECTION SUCCESSFUL")
+  }); 
+  ready();
+}
+function showMessage(data){
+  let ul = document.getElementById("messages");
+  let li = document.createElement("li");
+  li.appendChild(document.createTextNode(data));
+  ul.appendChild(li);
+}
+peer.on('connection', function(connection) {
+  showMessage("SOMEONE JUST CONNECTED "+connection.peer);
+  // connection.on('data', function(data){
+  //   console.log(data);
+  //   showMessage(data);   
+  // });
+  conn = connection;
+  ready();
+});
+function ready(){
+  conn.on('data', function(data){
+    console.log(data);
+    showMessage(data);   
+  })
+}
+let sendButton = document.getElementById("send");
+sendButton.addEventListener('click',sendMessage);
+function sendMessage(){
+  let message = document.getElementById("message");
+  if(message.value != null){
+    conn.send(message.value);
+  }
+  message.value = null;
+}
